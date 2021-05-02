afc8254157e886fc822bc5730d0f1c3a7f30e4b3
sinyoung yun <54181528+yenow@users.noreply.github.com>
2021-05-02 21:02:44 +0900

    next,js start

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';  //  next.js에서는 이게 필요가 없음,  아직 좀더 봐야함(주석하니까 실행이 안됬음)
+
+const Home = () => {
+    return (
+        <div>Hello, Next!</div>
+    );
+}
+
+export default Home;
