2f0458b136ebf34b63e788828aa5e07eb71b7d98
sinyoung yun <54181528+yenow@users.noreply.github.com>
2021-07-13 23:36:14 +0900

    UI compose

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,26 +1,48 @@
-import propTypes from 'prop-types';
+import React from 'react';
 import Link from 'next/link';
+import PropTypes from 'prop-types';
+import { Col, Input, Menu, Row } from 'antd';
+
+import LoginForm from './LoginForm';
+import UserProfile from './UserProfile';
+
+const dummy = {
+  nickname: '제로초',
+  Post: [],
+  Followings: [],
+  Followers: [],
+  isLoggedIn: false,
+};
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <Link href="/">
-        <a>노드버드</a>
-      </Link>
-      <Link href="/profile">
-        <a>프로필</a>
-      </Link>
-      <Link href="/signup">
-        <a>회원가입</a>
-      </Link>
-      {children}
+      <Menu mode="horizontal">
+        <Menu.Item key="home"><Link href="/"><a>노드버드</a></Link></Menu.Item>
+        <Menu.Item key="profile"><Link href="/profile"><a>프로필</a></Link></Menu.Item>
+        <Menu.Item key="mail">
+          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {dummy.isLoggedIn
+            ? <UserProfile />
+            : <LoginForm />}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href="https://www.zerocho.com" target="_blank" rel="noreferrer noopener">Made by ZeroCho</a>
+        </Col>
+      </Row>
     </div>
   );
 };
 
 AppLayout.propTypes = {
-  children: propTypes.node.isRequired,
-  // node : 리액트의 노드, 화면에 그릴수 있는 모든것?
-};
+  children: PropTypes.node.isRequired,
+}
 
 export default AppLayout;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,39 @@
-import AppLayout from "../components/AppLayout";
+import React from 'react';
+import PostForm from '../components/PostForm';
+import PostCard from '../components/PostCard';
+import AppLayout from '../components/AppLayout';
+
+const dummy = {
+  isLoggedIn: true,
+  imagePaths: [],
+  mainPosts: [{
+    id: 1,
+    User: {
+      id: 1,
+      nickname: '제로초',
+    },
+    content: '첫 번째 게시글',
+    Images: [{
+      src: 'https://bookthumb-phinf.pstatic.net/cover/137/995/13799585.jpg?udate=20180726',
+    }, {
+      src: 'https://gimg.gilbut.co.kr/book/BN001958/rn_view_BN001958.jpg',
+    }, {
+      src: 'https://gimg.gilbut.co.kr/book/BN001998/rn_view_BN001998.jpg',
+    }]
+  }],
+};
 
 const Home = () => {
-    return (
-
-        <AppLayout>
-            <div>Hello.Next!</div>
-        </AppLayout>
-    );
-}
+  return (
+    <AppLayout>
+      {dummy.isLoggedIn && <PostForm />}
+      {dummy.mainPosts.map((c) => {
+        return (
+          <PostCard key={c.id} post={c} />
+        );
+      })}
+    </AppLayout>
+  );
+};
 
 export default Home;
